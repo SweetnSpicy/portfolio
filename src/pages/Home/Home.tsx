e9c134bd58898889601d7d0f99d9008bf39fac76
Frederic-Yao <fredericyao@outlook.com>
2026-01-23 09:11:27 -0500

    Added content to Fred-Resume

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,4 @@
 
-import './Home.scss'
-
 /** 
  * main page file 
 */
--- src/pages/Resume/FredResume.tsx
+++ src/pages/Resume/FredResume.tsx
@@ -3,7 +3,14 @@ import './Resume.scss'
 function FredResume() {
     return (
         <div>
-            <h2>FredrickDouglas</h2>
+            <h1>Fredric Yao</h1>
+            <p> 819-923-4238 | fredericyao@outlook.com | My LinkedIn |
+                 My Github 
+            </p>
+            <h2>Education</h2>
+            <h2>Experience</h2>
+            <h2>Projects</h2>
+            <h2>Technical Skills</h2>
         </div>
     );
 }
